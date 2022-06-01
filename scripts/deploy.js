3b2b06976b353e50e202a376565a20fb87ce0296
Florian Meiswinkel <florian.meiswinkel@outlook.de>
2022-06-01 17:13:52 +0200

    Initial Commit

---
+++ scripts/deploy.js
@@ -0,0 +1,40 @@
+const { ethers, run, network } = require("hardhat")
+
+async function main() {
+    const simpleStorageFactory = await ethers.getContractFactory(
+        "SimpleStorage"
+    )
+
+    console.log("Deploying contract...")
+    const simpleStorage = await simpleStorageFactory.deploy()
+    await simpleStorage.deployed()
+
+    console.log(`Deployed contract to ${simpleStorage.address}`)
+
+    if (network.config.chainId === 4 && process.env.ETHERSCAN_API_KEY) {
+        await verify(simpleStorage.address)
+    }
+}
+
+async function verify(contractAddress, args) {
+    console.log("Verifying contract...")
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructor: args,
+        })
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Already verified!")
+        } else {
+            console.log(e)
+        }
+    }
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
